481787e8e61393c5e5c7552a57af2b35e35e740f
leunamanuelgc <121983846+leunamanuelgc@users.noreply.github.com>
2026-03-18 12:37:02 +0100

    Add transparent to canvas + fill gaps
    The canvas is now rendered with DrawTextureTransparent to show the alpha bg.
    
    Also, added the Bresenham's line algorithm to fill the gaps between frames when painting faster.

--- draw_plugin/Assets/Editor/RasterGraphicsEditorWindow.cs
+++ draw_plugin/Assets/Editor/RasterGraphicsEditorWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using Unity.VisualScripting;
 
 public class RasterGraphicsEditorWindow : EditorWindow
 {
@@ -48,8 +49,20 @@ public class RasterGraphicsEditorWindow : EditorWindow
         canvas_height = canvas_width * aspect_ratio;
         rect_canvas = new Rect(300, 200, canvas_width, canvas_height);
 
-        texture = new Texture2D((int)Mathf.Ceil(canvas_width), (int)Mathf.Ceil(canvas_height));
+        int iCanvasWidth = (int)Mathf.Ceil(canvas_width);
+        int iCanvasHeight = (int)Mathf.Ceil(canvas_height);
+        texture = new Texture2D(iCanvasWidth, iCanvasHeight, TextureFormat.RGBA32, false);
+        texture.alphaIsTransparency = true;
+        Color[] colors = new Color[iCanvasWidth * iCanvasHeight];
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = new Color(0, 0, 0, 0);
+        }
+
+        texture.SetPixels(0, 0, iCanvasWidth, iCanvasHeight, colors);
+        texture.Apply();
         texture_name = texture.name;
+        Repaint();
 
         current_color = Color.black;
 
@@ -86,12 +99,6 @@ public class RasterGraphicsEditorWindow : EditorWindow
         EditorGUILayout.EndVertical();
     }
 
-    bool isMouseInsideRect(Rect rect_to_check)
-    {
-        return (Event.current.mousePosition.x > rect_to_check.x && Event.current.mousePosition.x < rect_to_check.x + rect_to_check.width)
-            && (Event.current.mousePosition.y > rect_to_check.y && Event.current.mousePosition.y < rect_to_check.y + rect_to_check.height);
-    }
-
     void displayMainMenuToolbar()
     {
         EditorGUILayout.BeginHorizontal();
@@ -121,7 +128,7 @@ public class RasterGraphicsEditorWindow : EditorWindow
         if (EditorGUILayout.DropdownButton(new GUIContent("Image"), FocusType.Keyboard, EditorStyles.toolbarButton))
         {
             GenericMenu menu = new GenericMenu();
-
+                
             menu.ShowAsContext();
         }
 
@@ -202,13 +209,22 @@ public class RasterGraphicsEditorWindow : EditorWindow
 
     void eraserToolOptions()
     {
-        if (EditorGUILayout.DropdownButton(new GUIContent("Option 1"), FocusType.Keyboard, EditorStyles.toolbarButton))
+        string[] brushTypeOptionsList = { BrushShapeType.box.ToString(), BrushShapeType.rect.ToString() };
+        brush_type_index = EditorGUILayout.Popup(brush_type_index, brushTypeOptionsList);
+
+        EditorGUILayout.PrefixLabel("Eraser size");
+
+        switch (brush_type_index)
         {
-            GenericMenu menu = new GenericMenu();
-
-            menu.AddItem(new GUIContent("Test/1"), true, test, 1);
-
-            menu.ShowAsContext();
+            case 0:
+                int brush_size = EditorGUILayout.IntSlider(new GUIContent(""), brush_vector_size.x, min_brush_size, max_brush_size);
+                brush_vector_size = new Vector2Int(brush_size, brush_size);
+                break;
+            case 1:
+                int brush_size_x = EditorGUILayout.IntSlider(new GUIContent("X"), brush_vector_size.x, min_brush_size, max_brush_size);
+                int brush_size_y = EditorGUILayout.IntSlider(new GUIContent("Y"), brush_vector_size.y, min_brush_size, max_brush_size);
+                brush_vector_size = new Vector2Int(brush_size_x, brush_size_y);
+                break;
         }
     }
 
@@ -218,6 +234,8 @@ public class RasterGraphicsEditorWindow : EditorWindow
 
         if (EditorGUILayout.DropdownButton(new GUIContent("select"), FocusType.Keyboard, EditorStyles.toolbarButton))
         {
+            var rect = GUILayoutUtility.GetLastRect();
+
             current_tool = ToolType.select;
         }
 
@@ -238,35 +256,85 @@ public class RasterGraphicsEditorWindow : EditorWindow
     {
         //EditorGUILayout.RectField(rect_canvas);
 
-        EditorGUI.DrawPreviewTexture(rect_canvas, texture, null, ScaleMode.ScaleToFit, (float)texture.width / (float)texture.height);
+        EditorGUI.DrawTextureTransparent(rect_canvas, texture, ScaleMode.ScaleToFit, (float)texture.width / (float)texture.height);
+
+        //GUI.DrawTexture(rect_canvas, texture, ScaleMode.ScaleToFit, true, (float)texture.width / (float)texture.height);
+
+        //EditorGUI.DrawPreviewTexture(rect_canvas, texture, null, ScaleMode.ScaleToFit, (float)texture.width / (float)texture.height);
 
         if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag)
         {
-            if (isMouseInsideRect(rect_canvas))
+            if (rect_canvas.Contains(Event.current.mousePosition) && current_tool == ToolType.brush)
             {
-                float x_mouse_pos_in_canvas = Event.current.mousePosition.x - rect_canvas.x;
-                float y_mouse_pos_in_canvas = rect_canvas.height - (Event.current.mousePosition.y - rect_canvas.y);
-
-                float x_canvas_convertion = (float)texture.width / (float)canvas_width;
-                float y_canvas_convertion = (float)texture.height / (float)canvas_height;
-
-                float x_pos_in_texture;
-                float y_pos_in_texture;
-
-                x_pos_in_texture = x_mouse_pos_in_canvas * x_canvas_convertion;
-                y_pos_in_texture = y_mouse_pos_in_canvas * y_canvas_convertion;
-
-                Color[] colors = new Color[brush_vector_size.x * brush_vector_size.y];
-                for (int i = 0; i < colors.Length; i++)
-                {
-                    colors[i] = current_color;
-                }
-
-                texture.SetPixels((int)x_pos_in_texture, (int)y_pos_in_texture, brush_vector_size.x, brush_vector_size.y, colors);
-                texture.Apply();
-                Repaint();
+                PaintInCanvas(current_color);
+            }
+            else if (rect_canvas.Contains(Event.current.mousePosition) && current_tool == ToolType.eraser)
+            {
+                Color eraserColor = new Color(0, 0, 0, 0);
+                PaintInCanvas(eraserColor);
             }
         }
+    }
+
+    void PaintInCanvas(Color color)
+    {
+        float xPosCanvas = Event.current.mousePosition.x - rect_canvas.x;
+        float yPosCanvas = rect_canvas.height - (Event.current.mousePosition.y - rect_canvas.y);
+
+        Vector2 delta = Event.current.delta;
+        
+        float prev_xPosCanvas = xPosCanvas - delta.x;
+        float prev_yPosCanvas = yPosCanvas + delta.y;
+
+        float x_canvas_convertion = (float)texture.width / (float)canvas_width;
+        float y_canvas_convertion = (float)texture.height / (float)canvas_height;
+
+        float x1 = xPosCanvas * x_canvas_convertion;
+        float y1 = yPosCanvas * y_canvas_convertion;
+        float x0 = prev_xPosCanvas * x_canvas_convertion;
+        float y0 = prev_yPosCanvas * y_canvas_convertion;
+
+        float x = x0;
+        float y = y0;
+        float deltaX = (x1 - x0);
+        float deltaY = (y1 - y0);
+        float mY = deltaY / deltaX;
+        float mX = deltaX / deltaY;
+
+        Color[] colors;
+
+        while (x != x1 || y != y1)
+        {
+            colors = new Color[brush_vector_size.x * brush_vector_size.y];
+            for (int j = 0; j < colors.Length; j++)
+            {
+                colors[j] = color;
+            }
+            texture.SetPixels((int)x, (int)y, brush_vector_size.x, brush_vector_size.y, colors);
+
+            // I'm using Bresenham's line algorithm to fill gaps between painting frames
+            if (Mathf.Abs(deltaX) >= Mathf.Abs(deltaY)) {
+                if (x0 != x1)
+                    x += x0 < x1 ? 1 : -1;
+                y = mY * (x - x0) + y0;
+            }
+            else
+            {
+                if (y0 != y1)
+                    y += y0 < y1 ? 1 : -1;
+                x = mX * (y - y0) + x0;
+            }   
+        }
+
+        colors = new Color[brush_vector_size.x * brush_vector_size.y];
+        for (int j = 0; j < colors.Length; j++)
+        {
+            colors[j] = color;
+        }
+        texture.SetPixels((int)x, (int)y, brush_vector_size.x, brush_vector_size.y, colors);
+
+        texture.Apply();
+        Repaint();
     }
 
     void displayFunctionsToolbar()
