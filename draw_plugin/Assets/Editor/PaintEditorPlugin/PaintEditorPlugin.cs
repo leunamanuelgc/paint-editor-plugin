124db94041a80ee07033437fc1883dfabc3fb0c7
leunamanuelgc <121983846+leunamanuelgc@users.noreply.github.com>
2026-03-19 14:42:35 +0100

    Change Editor name and Add PaintCanvas class

--- draw_plugin/Assets/Editor/PaintEditorPlugin/PaintEditorPlugin.cs
+++ draw_plugin/Assets/Editor/PaintEditorPlugin/PaintEditorPlugin.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
-using Unity.VisualScripting;
-using static log4net.Appender.ColoredConsoleAppender;
+using UnityEngine.UI;
+using System;
 
-public class RasterGraphicsEditorWindow : EditorWindow
+
+public class PaintEditorPlugin : EditorWindow
 {
+    PaintCanvas canvas;
+
     enum ToolType
     {
         select,
@@ -16,12 +19,6 @@ public class RasterGraphicsEditorWindow : EditorWindow
     }
 
     ToolType current_tool;
-
-    public Texture2D texture;
-    string texture_name;
-
-    float aspect_ratio, canvas_width, canvas_height;
-    Rect rect_canvas;
 
     Color current_color;
     
@@ -37,33 +34,24 @@ public class RasterGraphicsEditorWindow : EditorWindow
     
 
     [MenuItem("Tools/Raster Editor")]
-    public static void CreateExample()
+    public static void CreateEditorWindow()
     {
-        EditorWindow.GetWindow<EditorWindow>();
-        EditorWindow.GetWindow(typeof(RasterGraphicsEditorWindow));
+        GetWindow<EditorWindow>();
+        GetWindow(typeof(PaintEditorPlugin));
     }
 
     public void OnEnable()
     {
-        aspect_ratio = 1f;
-        canvas_width = 256f;
-        canvas_height = canvas_width * aspect_ratio;
-        rect_canvas = new Rect(300, 200, canvas_width, canvas_height);
-
-        int iCanvasWidth = (int)Mathf.Ceil(canvas_width);
-        int iCanvasHeight = (int)Mathf.Ceil(canvas_height);
-        texture = new Texture2D(iCanvasWidth, iCanvasHeight, TextureFormat.RGBA32, false);
+        Vector2 canvasPosition = new Vector2(this.position.width / 2, this.position.height / 2);
+        Rect rect = new Rect(canvasPosition.x, canvasPosition.y, 256, 256);
+        Texture2D texture = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.ARGB32, true, false);
         texture.alphaIsTransparency = true;
-        Color[] colors = new Color[iCanvasWidth * iCanvasHeight];
-        for (int i = 0; i < colors.Length; i++)
-        {
-            colors[i] = new Color(0, 0, 0, 0);
-        }
-
-        texture.SetPixels(0, 0, iCanvasWidth, iCanvasHeight, colors);
+        Color[] colors = new Color[(int)rect.width * (int)rect.height];
+        Array.Fill(colors, new Color(0, 0, 0, 0));
+        texture.SetPixels(0, 0, (int)rect.width, (int)rect.height, colors);
         texture.Apply();
-        texture_name = texture.name;
-        Repaint();
+
+        canvas = new PaintCanvas(rect, texture);
 
         current_color = Color.black;
 
@@ -73,10 +61,13 @@ public class RasterGraphicsEditorWindow : EditorWindow
         brush_type_index = 0;
 
         current_tool = ToolType.brush;
+
+        Repaint();
     }
 
     public void OnGUI()
     {
+        Debug.Log(position);
         EditorGUILayout.BeginVertical();
 
         displayMainMenuToolbar();
@@ -255,17 +246,12 @@ public class RasterGraphicsEditorWindow : EditorWindow
 
     void displayCanvas()
     {
-        //EditorGUILayout.RectField(rect_canvas);
 
-        EditorGUI.DrawTextureTransparent(rect_canvas, texture, ScaleMode.ScaleToFit, (float)texture.width / (float)texture.height);
-
-        //GUI.DrawTexture(rect_canvas, texture, ScaleMode.ScaleToFit, true, (float)texture.width / (float)texture.height);
-
-        //EditorGUI.DrawPreviewTexture(rect_canvas, texture, null, ScaleMode.ScaleToFit, (float)texture.width / (float)texture.height);
+        EditorGUI.DrawTextureTransparent(canvas.rect, canvas.texture, ScaleMode.ScaleToFit, canvas.texture.width / canvas.texture.height);
 
         if (Event.current.type == EventType.MouseDown || Event.current.type == EventType.MouseDrag)
         {
-            //rect_canvas.Contains(Event.current.mousePosition) &&
+            //canvas.Rect.Contains(Event.current.mousePosition) &&
             if ( current_tool == ToolType.brush)
             {
                 PaintInCanvas(current_color);
@@ -280,16 +266,16 @@ public class RasterGraphicsEditorWindow : EditorWindow
 
     void PaintInCanvas(Color color)
     {
-        float xPosCanvas = Event.current.mousePosition.x - rect_canvas.x;
-        float yPosCanvas = rect_canvas.height - (Event.current.mousePosition.y - rect_canvas.y);
+        float xPosCanvas = Event.current.mousePosition.x - canvas.rect.x;
+        float yPosCanvas = canvas.rect.height - (Event.current.mousePosition.y - canvas.rect.y);
 
         Vector2 delta = Event.current.delta;
         
         float prev_xPosCanvas = xPosCanvas - delta.x;
         float prev_yPosCanvas = yPosCanvas + delta.y;
 
-        float x_canvas_convertion = (float)texture.width / (float)canvas_width;
-        float y_canvas_convertion = (float)texture.height / (float)canvas_height;
+        float x_canvas_convertion = canvas.texture.width / canvas.rect.width;
+        float y_canvas_convertion = canvas.texture.height / canvas.rect.height;
 
         float x1 = xPosCanvas * x_canvas_convertion;
         float y1 = yPosCanvas * y_canvas_convertion;
@@ -302,8 +288,6 @@ public class RasterGraphicsEditorWindow : EditorWindow
         float deltaY = (y1 - y0);
         float mY = deltaY / deltaX;
         float mX = deltaX / deltaY;
-
-        int limit = 0;
 
         //Debug.Log("x0: " + x0 + ", y0: " + y0 + ", x: " + x + ", y: " + y + ", x1: " + x1 + ", y1: " + y1);
 
@@ -348,7 +332,7 @@ public class RasterGraphicsEditorWindow : EditorWindow
         }
 
         PaintPixels(color, x, y, brush_vector_size.x, brush_vector_size.y);
-        texture.Apply();
+        canvas.texture.Apply();
         Repaint();
     }
 
@@ -356,12 +340,12 @@ public class RasterGraphicsEditorWindow : EditorWindow
     {
         Rect point = new Rect(x - sizeX / 2, y - sizeY / 2, sizeX, sizeY);
 
-        if(pointTouchCanvas(point, rect_canvas))
+        if(pointTouchCanvas(point, canvas.rect))
         {
             point.xMin = Mathf.Max(point.xMin, 0);
             point.yMin = Mathf.Max(point.yMin, 0);
-            point.xMax = Mathf.Min(point.xMax, canvas_width);
-            point.yMax = Mathf.Min(point.yMax, canvas_height);
+            point.xMax = Mathf.Min(point.xMax, canvas.rect.width);
+            point.yMax = Mathf.Min(point.yMax, canvas.rect.height);
 
             Color[] colors;
             colors = new Color[(int)point.width * (int)point.height];
@@ -369,7 +353,7 @@ public class RasterGraphicsEditorWindow : EditorWindow
             {
                 colors[j] = color;
             }
-            texture.SetPixels((int)point.x, (int)point.y, (int)point.width, (int)point.height, colors);
+            canvas.texture.SetPixels((int)point.x, (int)point.y, (int)point.width, (int)point.height, colors);
         }
     }
 
@@ -394,7 +378,7 @@ public class RasterGraphicsEditorWindow : EditorWindow
 
         current_color = EditorGUILayout.ColorField(new GUIContent("Color"), current_color, true, true, true);
 
-        //texture = (Texture2D)EditorGUILayout.ObjectField(new GUIContent("Load texture"), texture, typeof(Texture2D), false);
+        //canvas.Texture = (Texture2D)EditorGUILayout.ObjectField(new GUIContent("Load texture"), canvas.Texture, typeof(Texture2D), false);
 
         EditorGUILayout.EndVertical();
     }
@@ -409,14 +393,13 @@ public class RasterGraphicsEditorWindow : EditorWindow
         Texture2D loadedTexture = new Texture2D(1, 1);
         ImageConversion.LoadImage(loadedTexture, rawImageData);
 
-        texture = new Texture2D(loadedTexture.width, loadedTexture.height, loadedTexture.format, true);
-        Graphics.CopyTexture(loadedTexture, texture);
+        canvas.texture = new Texture2D(loadedTexture.width, loadedTexture.height, loadedTexture.format, true);
+        Graphics.CopyTexture(loadedTexture, canvas.texture);
 
-        aspect_ratio = (float)texture.width / (float)texture.height;
-        canvas_width = 256f;
-        canvas_height = canvas_width / aspect_ratio;
-        rect_canvas.width = canvas_width;
-        rect_canvas.height = canvas_height;
+        float aspect_ratio = canvas.texture.width / canvas.texture.height;
+        float canvas_width = 256f;
+        float canvas_height = canvas_width / aspect_ratio;
+        canvas.rect = new Rect(300, 200, canvas_width, canvas_height);
     }
 
     void saveImage()
@@ -425,7 +408,7 @@ public class RasterGraphicsEditorWindow : EditorWindow
 
         if (path.Length != 0)
         {
-            byte[] bytes = texture.EncodeToPNG();
+            byte[] bytes = canvas.texture.EncodeToPNG();
             File.WriteAllBytes(path, bytes);
         }
     }
