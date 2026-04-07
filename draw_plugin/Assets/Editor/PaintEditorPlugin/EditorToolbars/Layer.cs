fdde7afb45975ea79a4d145c8535b99ad29b484c
leunamanuelgc <121983846+leunamanuelgc@users.noreply.github.com>
2026-04-07 16:41:37 +0200

    Add Layers GUI. Painting in different layers implementation still left

---
+++ draw_plugin/Assets/Editor/PaintEditorPlugin/EditorToolbars/Layer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace UnityEditor.PaintEditor
+{
+    public class Layer
+    {
+        public static string iconTextureOn = "d_VisibilityOn";
+        public static string iconTextureOff = "d_VisibilityOff";
+
+        public Texture2D texture { get; set; }
+
+        public bool isEnabled { get; set; }
+
+        public string name { get; set; }
+
+        public Layer()
+        {
+            var app = PaintEditorPlugin.Instance;
+            texture = new Texture2D((int)app.canvas.size.x, (int)app.canvas.size.y, TextureFormat.ARGB32, true, false);
+            isEnabled = true;
+            name = "New layer";
+        }
+
+    }
+}
--- draw_plugin/Assets/Editor/PaintEditorPlugin/EditorToolbars/Utils.cs
+++ draw_plugin/Assets/Editor/PaintEditorPlugin/EditorToolbars/Utils.cs
@@ -1,4 +1,6 @@
+using UnityEditorInternal;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace UnityEditor.PaintEditor
 {
@@ -12,12 +14,33 @@ namespace UnityEditor.PaintEditor
 
         public Color currentColor { get; set; }
 
+        public ReorderableList layers;
+
+        internal List<Layer> layerList { get; set; }
+
         public Utils()
         {
             var app = PaintEditorPlugin.Instance;
             width = 200;
             height = 300;
             rect = new Rect(app.position.width - (width + 20), 100, width, height);
+
+            layerList = new List<Layer>();
+
+            layers = new ReorderableList(layerList, typeof(Layer), true, true, true, true);
+            layers.drawElementCallback = DrawLayers;
+            layers.drawHeaderCallback = DrawHeader;
+            layers.onAddCallback = AddLayer;
+            layers.onRemoveCallback = RemoveLayer;
+            layers.elementHeightCallback = (int index) => EditorGUIUtility.singleLineHeight + 10;
+
+
+            //m_ReorderableList.drawElementCallback = DrawSpriteSecondaryTextureElement;
+            //m_ReorderableList.onAddCallback = AddSpriteSecondaryTextureElement;
+            //m_ReorderableList.onRemoveCallback = RemoveSpriteSecondaryTextureElement;
+            //m_ReorderableList.onCanAddCallback = CanAddSpriteSecondaryTextureElement;
+            //m_ReorderableList.elementHeightCallback = (int index) => (EditorGUIUtility.singleLineHeight * 3) + 5;
+            //m_ReorderableList.onSelectCallback = OnSelectCallback;
         }
 
         public void DisplayGUI()
@@ -33,6 +56,39 @@ namespace UnityEditor.PaintEditor
         private void CreateWindow(int id)
         {
             currentColor = EditorGUILayout.ColorField(new GUIContent("Color"), currentColor, true, true, true);
+
+            EditorGUILayout.Space(20);
+
+            layers.DoLayoutList();
+        }
+
+        private void DrawLayers(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            var icon = layerList[index].isEnabled? Layer.iconTextureOn : Layer.iconTextureOff;
+
+            if (GUI.Button(new Rect(rect.x, rect.y, 25, 25), new GUIContent((Texture)EditorGUIUtility.Load(icon))))
+            {
+                var newValue = !layerList[index].isEnabled;
+                layerList[index].isEnabled = newValue;
+            }
+            
+            layerList[index].name = EditorGUI.TextField(new Rect(rect.x + 30, rect.y + 4, 100, EditorGUIUtility.singleLineHeight), layerList[index].name);
+        }
+
+        private void DrawHeader(Rect rect)
+        {
+            string name = "Layers";
+            EditorGUI.LabelField(rect, name);
+        }
+
+        private void AddLayer(ReorderableList list)
+        {
+            layerList.Add(new Layer());
+        }
+
+        private void RemoveLayer(ReorderableList list)
+        {
+
         }
     }
 }
